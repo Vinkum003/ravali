083859a57232675de1fa3104225b9c88279139e5
Vinkum003 <61989816+Vinkum003@users.noreply.github.com>
2020-03-31 12:24:23 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,13 +1,7 @@
-# Get three test score
-round1 = int(raw_input("Enter score for round 1: "))
-
-round2 = int(raw_input("Enter score for round 2: "))
-
-round3 = int(raw_input("Enter score for round 3: "))
-   
-# Calculate the average
-average = (round1 + round2 + round3) / 3
-
-# Print out the test score
-print("the average score is: ", average)
-
+n=int(input("Enter a number:"))
+tot=0
+while(n>0):
+    dig=n%10
+    tot=tot+dig
+    n=n//10
+print("The total sum of digits is:",tot)
